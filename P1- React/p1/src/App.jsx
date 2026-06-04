c1ad99521faeef4b4a45fe0d4c2165d84c882a9f
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-06-04 17:25:52 -0300

    Primeiro commit

---
+++ P1- React/p1/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+  
+  return (
+    <section className='container'>
+      <h1>Disney!</h1>
+
+      <div className='card'>
+        <h1>Digite o seu e-mail para continuar</h1>
+      </div>
+
+    </section>
+    
+  )
+}
+
+export default App
